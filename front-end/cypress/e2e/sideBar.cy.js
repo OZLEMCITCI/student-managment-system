a7b2dbda33c92f3049870c8fc260162d56c689b1
OZLEMCITCI <ozlmctc@gmail.com>
2023-04-03 14:50:07 -0500

    first cypress test

---
+++ front-end/cypress/e2e/sideBar.cy.js
@@ -0,0 +1,36 @@
+describe('Sidebar Extension', () => {
+
+    beforeEach(()=>{
+        cy.mockStudents()
+        cy.visit('http://localhost:3000/') // Replace this with the URL of your application
+        cy.wait('@getStudents') // Wait for the mock API response to load
+    })
+
+    it('should toggle collapse when clicked', () => {
+        cy.get('.ant-layout-sider-trigger').click()
+        cy.get('.ant-layout-sider').should('have.class', 'ant-layout-sider-collapsed')
+        cy.get('.ant-layout-sider-trigger').click()
+        cy.get('.ant-layout-sider').should('not.have.class', 'ant-layout-sider-collapsed')
+    })
+
+
+    it('should open submenu when clicked', () => {
+        const initialUrl = Cypress.config().baseUrl;
+        cy.get('.ant-menu-submenu-title').contains('User').click()
+        cy.get('.ant-menu-submenu-open').should('have.length', 1)
+        cy.get('.ant-menu-item').contains('Tom').click()
+        cy.url().should('not.contain', '/#3')
+        cy.get('.ant-menu-item').contains('Bill').click()
+        cy.url().should('not.contain', '/#4')
+        cy.get('.ant-menu-item').contains('Alex').click()
+        cy.url().should('not.contain', '/#5')
+        cy.get('.ant-menu-submenu-title').contains('Team').click()
+        cy.get('.ant-menu-submenu-open').should('have.length', 2)
+        cy.get('.ant-menu-item').contains('Team 1').click()
+        cy.url().should('not.contain', '/#6')
+        cy.get('.ant-menu-item').contains('Team 2').click()
+        cy.url().should('not.contain', '/#8')
+
+    })
+})
+
--- front-end/src/components/layout.js
+++ front-end/src/components/layout.js
@@ -55,12 +55,6 @@ function AppLayout ({students,fetching}) {
                onCollapse={setCollapsed}>
             <div className="logo" />
             <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                <Menu.Item key="1" icon={<PieChartOutlined />}>
-                    Option 1
-                </Menu.Item>
-                <Menu.Item key="2" icon={<DesktopOutlined />}>
-                    Option 2
-                </Menu.Item>
                 <SubMenu key="sub1" icon={<UserOutlined />} title="User">
                     <Menu.Item key="3">Tom</Menu.Item>
                     <Menu.Item key="4">Bill</Menu.Item>
